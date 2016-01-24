3e5853c4b7d6fd79937d74e3f84e80c563dd4da2
jlangensand15 <jlangensand@gmail.com>
2016-01-24 15:04:43 -0800

    Update instagram-query.js

--- instagram-query.js
+++ instagram-query.js
@@ -5,9 +5,7 @@ var access_token = "2205178294.324cf62.a569c4db3a394908bfa806cfafae2397";
        access_parameters = {
            access_token: access_token
        }
-
 //sample query URL https://api.instagram.com/v1/tags/fail/media/recent?count=5&client_id=144a084754754ee393eafd7814c787d6
-
 
 // Attach an event listener to the form submit (using jQuery)
 $("#tag-search-form").submit(formSubmitted);
@@ -15,33 +13,34 @@ $("#tag-search-form").submit(formSubmitted);
 // Handle the form submission: go to Instagram and get results
 function formSubmitted(event) {
   event.preventDefault();
-  var url = "https://api.instagram.com/v1/tags/travel" +$("#query").val() + "/media/recent?count=5&access_token=2205178294.324cf62.a569c4db3a394908bfa806cfafae2397";
+  var url = "https://api.instagram.com/v1/tags/travel" +$("#query").val() + "/media/recent?count=6&access_token=2205178294.324cf62.a569c4db3a394908bfa806cfafae2397";
   $.get(url, resultsReceived);
   console.log(url);
 }
 
 function resultsReceived(results) {
-// foreach.reslts(getimages)
-console.log(results["data"].length);
+  console.log(results);
+  console.log(results["data"].length);./
 
   // results.forEach(loadimage);
   for (var i = 0; i < results["data"].length; i++){
     console.log(i);
     var imagesrc = results["data"][i]["images"]["thumbnail"]["url"];
-''
-
     var caption = results["data"][i]["caption"]["text"];
+    var link = results["data"][i]["link"];
 
   // funciton loadimage(results["data"]) {
-  // console.log("working here??");
-  var imageList = document.querySelector("#image");
-  var li = document.createElement("li");
-  var imageDiv = document.createElement("div");
-  var link = document.createElement("a");
-  var description = document.createElement("div");
+    var imageList = document.querySelector("#image");
+    var imageDiv = document.createElement("div");
+    var hyperlink = document.createElement("a");
+    var description = document.createElement("div");
+    var image=document.createElement("img");
 
-  var image=document.createElement("img");
+  hyperlink.setAttribute("href", link);
+  hyperlink.setAttribute("target", "_blank");
   image.setAttribute("src", imagesrc);
-  imageList.appendChild(image);
+  image.setAttribute("title", caption);
+  hyperlink.appendChild(image);
+  imageList.appendChild(hyperlink);
 };
 };
